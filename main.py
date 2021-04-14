b8adbebb3489cea75c3363715e18849d4ee1a975
Ajay Kahar <kaharajay8@gmail.com>
2021-04-14 18:11:44 +0530

    Face detection code

---
+++ main.py
@@ -0,0 +1,32 @@
+import cv2
+
+def fun():
+ is_face_detected = False
+ # Load the cascade
+ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Read the input image
+ video = cv2.VideoCapture(0)
+ _, img = video.read()
+
+ # Convert into grayscale
+ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect faces
+ faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+# Draw rectangle around the faces
+ for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    if (w != 0):
+            is_face_detected = True
+    else:
+            is_face_detected = False
+ cv2.imshow('img', img)
+ cv2.waitKey(1000)
+
+ print(is_face_detected)
+# Display the output
+while True:
+ fun()
+ cv2.waitKey(1000)
